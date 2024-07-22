7885a6e502fc8733d3cf94e273ef7bc1485343aa
AsakuraEx <franescobar97@hotmail.com>
2024-07-21 21:21:17 -0600

    Se agrega una parte visual

---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <form>
+        <div class="campo">
+            <label for="nuevo-presupuesto">Definir Presupuesto</label>
+            <input 
+                type="text" 
+                id="nuevo-presupuesto" 
+                class="nuevo-presupuesto" 
+                placeholder="Añade tu presupuesto"
+            />
+        </div>
+        <input type="submit" value="Definir Presupuesto"/>
+    </form>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
